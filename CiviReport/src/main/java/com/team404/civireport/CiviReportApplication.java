afc31b800b5a7d7ef65a61b96227f679518027c6
viveksonawale <viveksonawale774@gmail.com>
2025-09-06 16:09:35 +0530

    Added Backend Structure

---
+++ CiviReport/src/main/java/com/team404/civireport/CiviReportApplication.java
@@ -0,0 +1,14 @@
+package com.team404.civireport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiviReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiviReportApplication.class, args);
+	}
+    
+}
+
---
+++ CiviReport/src/test/java/com/team404/civireport/CiviReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team404.civireport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiviReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
